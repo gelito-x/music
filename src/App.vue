dcdcc1d4221d98d631b6e6aa390620a0d17e2a23
Angel <angel_jadan@outlook.com>
2019-06-30 16:24:36 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,81 @@
+<template lang="pug">
+  #app
+    img(src='./assets/logo.png')
+    h1 Music
+    select(v-model="selectCountry") 
+      option(v-for="country in countries" v-bind:value="country.value") {{ country.name}} 
+    spinner(v-show="loading")
+    ul
+      artist(v-for="artist in artists" v-bind:artist="artist" v-bind:key="artist.mbid")
+</template>
+
+<script>
+import Spinner from './components/Spinner.vue'
+import Artist from './components/Artist.vue'
+import getArtists from './api'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      artists: [],
+      countries: [
+        { name: 'Ecuador', value:'ecuador'},
+        { name: 'Argentina', value:'argentina'},
+        { name: 'España', value:'spain'},
+        { name: 'Colombia', value:'colombia'}
+      ],
+      selectCountry: 'ecuador',
+      loading: true
+    }
+  },
+  methods:  {
+    refreshArtist: function(){
+      const self = this
+      self.artists = [];
+      self.loading = true;
+      getArtists(this.selectCountry)
+      .then(function (artists) {
+        self.loading = false;
+        self.artists = artists
+      })
+    }
+  },
+  mounted: function () {
+    this.refreshArtist();    
+  },
+  watch: {
+    selectCountry: function(){
+      this.refreshArtist();    
+    }
+  },
+  components: {
+    Artist,
+    Spinner
+  },
+}
+</script>
+
+<style lang="stylus">
+#app
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+  margin-top 60px
+
+h1, h2
+  font-weight normal
+
+ul
+  list-style-type none
+  padding 0
+
+li
+  display inline-block
+  margin 0 10px
+
+a
+  color #42b983
+</style>
